0fc43b2955a8ce25c105928a7a7e0571b55498af
TiagoB <tiagobrizuela18@gmail.com>
2022-06-11 23:28:36 -0300

    Primer maquetado

--- TP-BackFrontend-5044/frontEnd/Frontend/src/app/app-routing.module.ts
+++ TP-BackFrontend-5044/frontEnd/Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LibrosComponent } from './components/libros/libros.component';
+import { PasajesComponent } from './components/pasajes/pasajes.component';
+import { TransaccionComponent } from './components/transaccion/transaccion.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component:HomeComponent},
+  {path:'libros', component:LibrosComponent},
+  {path:'transacciones', component:TransaccionComponent},
+  {path:'pasajes', component:PasajesComponent},
+  {path:'**', pathMatch:'full', redirectTo:'home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- TP-BackFrontend-5044/frontEnd/Frontend/src/app/app.module.ts
+++ TP-BackFrontend-5044/frontEnd/Frontend/src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { LibrosComponent } from './components/libros/libros.component';
+import { TransaccionComponent } from './components/transaccion/transaccion.component';
+import { PasajesComponent } from './components/pasajes/pasajes.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    LibrosComponent,
+    TransaccionComponent,
+    PasajesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
